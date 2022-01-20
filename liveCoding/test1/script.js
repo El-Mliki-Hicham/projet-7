d570970395733009c7ca8b6f9b4780d4ff730189
EL.MLIKI.HICHAM <92031524+El-Mliki-Hicham@users.noreply.github.com>
2022-01-21 00:25:30 +0100

    add

--- liveCoding/test1/script.js
+++ liveCoding/test1/script.js
@@ -1,6 +1,6 @@
 var gestionOuvrage = new GestionOuvrage 
 var rowNew = null
-
+var rowId
 document.getElementById("formSubmit").addEventListener("submit" ,function (event) {
     event.preventDefault()
   
@@ -18,7 +18,7 @@ if(rowNew == null){
  function readOuvrage(ouvrage){
     ouvrage = new Ouvrage
 ouvrage.titre = document.getElementById("inputTitle").value
-ouvrage.prix = document.getElementById("inputTitle").value
+ouvrage.prix = document.getElementById("inputAuthor").value
 return ouvrage 
  }
 
@@ -38,6 +38,35 @@ for ( i = 0; i < list.length; i++) {
     cell2= newRow.insertCell(1)
     cell2.innerHTML = list[i].titre 
     
-}
+    cell3 = newRow.insertCell(2)
+
+    var modifierButton = document.createElement('button')
+    var supprimeButton = document.createElement('button')
+
+    var buttonNameModifier = document.createTextNode("modifier")
+    var buttonNameSupprime = document.createTextNode("supprime")
+
+
+    modifierButton.appendChild(buttonNameModifier)
+    supprimeButton.appendChild(buttonNameSupprime)
+
+    modifierButton.setAttribute("onclick",'modifier(this)')
+    supprimeButton.setAttribute("onclick",'supprime(this)')
+
+    cell3.appendChild(modifierButton)
+    cell3.appendChild(supprimeButton)
+
+}}
+
+function modifier(buttonReferance){
+
+rowNew = buttonReferance.parentElement.parentElement
+
+rowId = gestionOuvrage.getItems(id)
+
+
+document.getElementById('inputAuthor').value = ouvrage.prix
+document.getElementsById("inputTitle").value = ouvrage.titre
+
 
 }
